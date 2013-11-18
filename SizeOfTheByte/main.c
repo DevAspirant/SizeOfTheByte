28a6b2977fadda5f27177243496e4896c7ad2140
ammar falmban <devAspirant@gmail.com>
2013-11-18 10:40:20 +0900

    Initial Commit

---
+++ SizeOfTheByte/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SizeOfTheByte
+//
+//  Created by ammar falmban on 11/18/13.
+//  Copyright (c) 2013 SDKaizen.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
